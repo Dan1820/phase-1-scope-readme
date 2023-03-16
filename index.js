e9e1c7338f8d2bbd31ec83c302c16a7d057623e8
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-16 18:31:30 +0300

    completed message

---
+++ index.js
@@ -0,0 +1,20 @@
+// function myFunc(){
+//     const myVar=42;
+//     return myVar* 2;
+//     // const myVar = myFunc() *2;
+// }
+
+// console.log(myFunc())
+if (true){
+    const myVar=43;
+     myOtherVar=9000;
+   
+}
+// console.log(myVar)
+console.log(myOtherVar)
+
+function bankAccount(){
+    secretPassword='I love puppies'
+    return 'bankAccount() function invoked';
+}
+console.log(bankAccount())
